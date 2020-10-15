16db8af6002c829c1cf77a81b7672abfe2289b61
jackyzhan2016 <jackyzhan@foxmail.com>
2020-10-15 15:26:02 +0800

    增加注释

--- handle.c
+++ handle.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 /* 协议的应用举例 */
+/* 验证消息 */
 int verify(struct base *bs)
 {
 	if (验证应用 ID：bs->appid 失败) {
@@ -10,15 +11,16 @@ int verify(struct base *bs)
 	}
 }
 
+/* 处理消息 */
 int handle_msg(union msg *msg)
 {
 	int type = verify(&msg->bs);
 	if (-1 == type) return -1;
 	switch (type) {
-	case CHAT_MSG:
+	case CHAT_MSG:  /* 聊天消息 */
 		handle_chat((struct msg *) msg);
 		break;
-	case LOGIN:
+	case LOGIN:     /* 登录消息 */
 		handle_login((struct login *) msg);
 		break;
 	}
